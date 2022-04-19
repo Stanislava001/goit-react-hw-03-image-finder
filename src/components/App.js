eb3790d28a33fee6b03f1020bacac288fff36343
Stanislava001 <86718839+Stanislava001@users.noreply.github.com>
2022-04-19 15:51:15 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -24,7 +24,7 @@ class App extends Component {
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.searchQuery !== this.state.searchQuery) {
-      this.fetchImgs;
+      this.fetchImgs();
     }
   }
 
