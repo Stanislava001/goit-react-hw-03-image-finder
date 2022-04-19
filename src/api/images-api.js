3ce8edb1104617aa367bc99cdd3a116eed396050
Stanislava001 <86718839+Stanislava001@users.noreply.github.com>
2022-04-19 15:49:35 +0300

    Update images-api.js

--- src/api/images-api.js
+++ src/api/images-api.js
@@ -2,14 +2,11 @@ import axios from 'axios';
 const API_KEY = '24835779-0f65b5f1ebc58d529319d0b23';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export function fetchImages(searchQuery = ' ', page = 1, perPage = 12) {
-  return axios
-    .get(
-      `${BASE_URL}?q=${searchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${perPage}`
-    )
-    .then(res => {
-      return res.data.hits;
-    });
+async function fetchImages(searchQuery = ' ', page = 1, perPage = 12) {
+  const res = await axios.get(
+    `${BASE_URL}?q=${searchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${perPage}`
+  );
+  return res.data.hits;
 }
 
 //res => res.data.images
